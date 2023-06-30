287f23d37c82dd2b8bf82f014f419758fa5f5d2a
David Martin <david.martin@split.io>
2023-06-30 08:51:11 -0500

    batch for impressions counts over 200

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const base64js = require('base64-js')
 const { URLSearchParams } = require('url');
 
 exports.handler = async(event) => {
-
+    // console.log(event);
     const encodedMixpanelToken = event["queryStringParameters"].m;
     const mixpanelSecret = Buffer.from(base64js.toByteArray(encodedMixpanelToken)).toString().trim();
 
@@ -32,23 +32,32 @@ exports.handler = async(event) => {
         events.push(event);
     }
 
-    const encodedParams = new URLSearchParams();
-    const mixpanelBody = JSON.stringify(events);
-    encodedParams.set('data', mixpanelBody);
-
-    await axios.post('http://api.mixpanel.com/track/', encodedParams, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'text/plain'
-            }
-        })
-        .then(function(response) {
-            console.log(response.data);
-        })
-        .catch(function(error) {
-                    console.log(error);
-        });
-
+    let counter = 0;
+    let batchSize = 200;
+    while(counter < events.length) {
+        
+        const batch = events.slice(counter, counter+batchSize < events.length ? counter + batchSize : events.length);
+        counter = counter + batchSize;
+        
+        const encodedParams = new URLSearchParams();
+        const mixpanelBody = JSON.stringify(batch);
+        encodedParams.set('data', mixpanelBody);
+    
+        await axios.post('http://api.mixpanel.com/track?verbose=1', encodedParams, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': 'text/plain'
+                }
+            })
+            .then(function(response) {
+                console.log('track success');
+                console.log(response.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    
+    }
     console.log('sent ' + events.length + ' events to MixPanel');
     
     const response = {
